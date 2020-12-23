273ece5b42107ea1b039b45d5dd734d35245881b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-23 15:38:35 +0100

    Create AppSecurityConfig.java

---
+++ src/main/java/com/tp/config/AppSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.tp.config;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails; // bibliothéque SP sec
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		// TODO Auto-generated method stub
+	List<UserDetails> users=new ArrayList<>();
+	users.add(User.withDefaultPasswordEncoder().username("khalil").password("admin").roles("user").build());
+	return new InMemoryUserDetailsManager(users);
+	}
+
+
+}
